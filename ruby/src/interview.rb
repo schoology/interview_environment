67ec42e0a23566b569d5cf880a764f600055c1cf
Alex Montgomery <amontgomery@schoology.com>
2014-07-17 23:09:23 -0400

    Add ruby box

--- ruby/src/interview.rb
+++ ruby/src/interview.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
+require 'mysql'
+require 'yaml'
 
 configure do
 	enable :logging
+	set :bind, '0.0.0.0'
 end
 
+DB_CONFIG = YAML::load(File.open('config/database.yml'))
+
+#http://rubylearning.com/blog/2007/05/14/ruby-mysql-tutorial/
+#my = Mysql.new(hostname, username, password, databasename)
+con = Mysql.new('localhost', DB_CONFIG['username'], DB_CONFIG['password'], 'interview')
+
 get '/' do
-  'Hello World!'
+  con.query("INSERT INTO wonks (title) VALUES('foo')");
+  erb :content
 end
