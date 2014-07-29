75715f14892219e883a49463540e0046c57edcfd
amontgomery <amontgomery@schoology.com>
2014-07-29 18:20:05 -0400

    Adding database connection to python box

--- python/src/interview.py
+++ python/src/interview.py
@@ -5,6 +5,8 @@ from web import template
 render = web.template.render('templates', base='layout')
 urls = ('/', 'index')
 app = web.application(urls, globals())
+db = web.database(dbn='mysql', user='interview', pw='interview', db='interview')
+
 
 class index:
 	def GET(self):
